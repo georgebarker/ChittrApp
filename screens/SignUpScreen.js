4b41a294e28086fae160f9ea35b18abb49087090
georgebarker <georgebarker1@me.com>
2020-02-21 23:34:43 +0000

    Implemented account creation

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,11 +1,158 @@
+/* global fetch */
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Button, StyleSheet, TextInput, Alert } from 'react-native'
 export default class SignUpScreen extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      givenName: null,
+      familyName: null,
+      email: null,
+      password: null,
+      isEmailValid: true
+    }
+  }
+
+  onSignUpPressed () {
+    if (this.isRequestValid()) {
+      fetch('http://10.0.2.2:3333/api/v0.0.5/user', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          given_name: this.state.givenName,
+          family_name: this.state.familyName,
+          email: this.state.email,
+          password: this.state.password
+        })
+      })
+        .then((response) => {
+          if (response.ok) {
+            this.handleSuccess()
+          } else {
+            this.handleFailure()
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+          this.handleFailure()
+        })
+    }
+  }
+
+  handleSuccess () {
+    Alert.alert('Success!',
+      'Your account was created successfully.',
+      [{ text: 'Log in', onPress: () => this.props.navigation.navigate('Login') }])
+  }
+
+  handleFailure () {
+    Alert.alert('Oops!',
+      'Something went wrong. Please try again.')
+  }
+
+  /** Checks if sign up request is valid.
+   * Will display error message UI indicators if requests are invalid. */
+  isRequestValid () {
+    var isRequestValid = true
+
+    Object.entries(this.state).forEach(([key, value]) => {
+      // Check for null or empty fields
+      if (value === null || value === '') {
+        isRequestValid = false
+        this.setState({ [key]: '' })
+      }
+      // Check for invalid email address
+      if (key === 'email' && !/\S+@\S+\.\S+/.test(value)) {
+        isRequestValid = false
+        this.setState({ isEmailValid: false })
+      }
+    })
+
+    return isRequestValid
+  }
+
+  displayError (prop) {
+    return prop !== '' ? { display: 'none' } : { display: 'flex' }
+  }
+
+  displayEmailError () {
+    return this.state.isEmailValid ? { display: 'none' } : { display: 'flex' }
+  }
+
   render () {
     return (
-      <View>
-        <Text>Sign Up screen</Text>
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+      >
+        <Text
+          style={{
+            fontSize: 40
+          }}
+        >Chittr
+        </Text>
+        <View style={styles.formContainer}>
+          <Text>Given name</Text>
+          <Text style={[styles.errorMessage, this.displayError(this.state.givenName)]}>Field is required</Text>
+          <TextInput
+            textContentType='givenName' maxLength={50} style={styles.textInput}
+            onChangeText={(text) => this.setState({ givenName: text })}
+          />
+          <Text>Family name</Text>
+          <Text style={[styles.errorMessage, this.displayError(this.state.familyName)]}>Field is required</Text>
+          <TextInput
+            textContentType='familyName' maxLength={50} style={styles.textInput}
+            onChangeText={(text) => this.setState({ familyName: text })}
+          />
+          <Text>Email address</Text>
+          <Text style={[styles.errorMessage, this.displayError(this.state.email)]}>Field is required</Text>
+          <Text style={[styles.errorMessage, this.displayEmailError()]}>Enter a valid email address (e.g. george@google.com)</Text>
+          <TextInput
+            textContentType='emailAddress' maxLength={320} style={styles.textInput}
+            onChangeText={(text) => this.setState({ email: text, isEmailValid: true })}
+          />
+          <Text>Password</Text>
+          <Text style={[styles.errorMessage, this.displayError(this.state.password)]}>Field is required</Text>
+          <TextInput
+            secureTextEntry textContentType='newPassword' maxLength={512} style={styles.textInput}
+            onChangeText={(text) => this.setState({ password: text })}
+          />
+        </View>
+        <View
+          style={styles.buttonContainer}
+        >
+          <Button
+            title='Sign up'
+            onPress={() => this.onSignUpPressed()}
+          />
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 100
+  },
+  formContainer: {
+    padding: 10,
+    margin: 20,
+    alignSelf: 'stretch'
+
+  },
+  textInput: {
+    borderColor: 'black',
+    backgroundColor: 'white',
+    borderWidth: 1,
+    marginBottom: 20
+  },
+  errorMessage: {
+    color: 'red'
+  }
+})
