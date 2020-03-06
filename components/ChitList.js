673efbba461002e47b6b3be5a0ef68b0b5c3bf95
georgebarker <georgebarker1@me.com>
2020-03-06 21:41:21 +0000

    Added in missing file from merge request on original branch

--- components/ChitList.js
+++ components/ChitList.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react'
-import { View, FlatList } from 'react-native'
+import { FlatList } from 'react-native'
 import HorizontalDivider from './HorizontalDivider'
 import Chit from './Chit'
 export default class ChitList extends Component {
   render () {
     return (
-      <View>
-        <FlatList
-          data={this.props.chits}
-          renderItem={({ item }) => <Chit chit={item} navigation={this.props.navigation} />}
-          keyExtractor={item => item.chit_id.toString()}
-          ItemSeparatorComponent={HorizontalDivider}
-          onRefresh={this.props.onRefresh}
-          refreshing={this.props.isLoading}
-        />
-      </View>
+      <FlatList
+        data={this.props.chits}
+        renderItem={({ item }) => <Chit chit={item} navigation={this.props.navigation} />}
+        keyExtractor={item => item.chit_id.toString()}
+        ItemSeparatorComponent={HorizontalDivider}
+        onRefresh={this.props.onRefresh}
+        refreshing={this.props.isLoading}
+        onEndReached={this.props.onEndReached}
+        onEndReachedThreshold={0.1}
+      />
     )
   }
 }
